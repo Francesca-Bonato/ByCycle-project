0182e776e2dcad2b64ef9660bd236b1d57ad1591
Francesca Bonato <francesca.bonato@hotmail.com>
2024-05-05 20:03:38 +0200

    add routing login

--- ByCycle/src/main.jsx
+++ ByCycle/src/main.jsx
@@ -3,10 +3,11 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
-import Homepage from "./pages/Homepage";
-import Forum from "./pages/Forum";
+import Community from "./pages/Community";
 import BikeTrails from "./pages/BikeTrails";
-import Newspage from "./pages/Newspage";
+import NewsPage from "./pages/NewsPage";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -14,9 +15,10 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { path: "/", element: <Homepage /> },
-      { path: "/forum", element: <Forum /> },
-      { path: "/piste", element: <BikeTrails/> },
-      { path: "/news", element: <Newspage /> },
+      { path: "/community", element: <Community/> },
+      { path: "/trails", element: <BikeTrails/> },
+      { path: "/news", element: <NewsPage /> },
+      { path: "/login", element: <Login /> },
     ],
   },
 ]);
