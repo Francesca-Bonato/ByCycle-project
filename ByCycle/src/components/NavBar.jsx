27e5029958c3d731f2f5e111fa96f9a115d76061
Francesco <fotifgerend@gmail.com>
2024-05-07 16:23:32 +0200

    Aggiunti commenti in English

--- ByCycle/src/components/NavBar.jsx
+++ ByCycle/src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logo from "../assets/images/logo.png";
+
+// Sample data for user and navigation
 const user = {
   name: "Tom Cook",
   email: "tom@example.com",
@@ -24,29 +26,45 @@ const userNavigation = [
   { name: "Sign out", href: "#" },
 ];
 
+// Utility function to manage component classes
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+// Main component
 export default function Example() {
+  // State to manage the profile menu display
   const [menuProfile, setMenuProfile] = useState(false);
 
+  // Handler to toggle the profile menu state
   function handlerOnProfile() {
     setMenuProfile(!menuProfile);
   }
 
+  // Render the component
   return (
     <>
+      {/* All Container */}
       <div className="min-h-full">
+        {/* Disclosure component to handle menu visibility */}
         <Disclosure as="nav" className="">
           {({ open }) => (
             <>
+              {/* Header */}
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
+                  {/* Logo */}
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img className="h-8 w-8" src={logo} alt="Your Company" />
+                      <a href="#">
+                        <img
+                          className="h-8 w-8"
+                          src={logo}
+                          alt="Your Company"
+                        />
+                      </a>
                     </div>
+                    {/* Desktop Navigation */}
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
@@ -64,20 +82,9 @@ export default function Example() {
                       </div>
                     </div>
                   </div>
+                  {/* Desktop Profile Menu Icon */}
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      {/* EVENTUALE BOTTONE NOTIFICHE descktop */}
-
-                      {/* <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button> */}
-
-                      {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
                         <div>
                           <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -90,6 +97,7 @@ export default function Example() {
                             />
                           </Menu.Button>
                         </div>
+                        {/* Profile Dropdown Menu */}
                         <Transition
                           as={Fragment}
                           enter="transition ease-out duration-100 transform"
@@ -120,8 +128,8 @@ export default function Example() {
                       </Menu>
                     </div>
                   </div>
+                  {/* Mobile Menu Icon */}
                   <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
                     <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md  p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-0.5" />
                       <span className="sr-only">Open main menu</span>
@@ -141,7 +149,7 @@ export default function Example() {
                 </div>
               </div>
 
-              {/* Mobile menu panel with Transition */}
+              {/* Mobile Menu Panel with Transition */}
               <Transition
                 as={Fragment}
                 enter="transition ease-out duration-200 transform"
@@ -185,20 +193,9 @@ export default function Example() {
                           {user.email}
                         </div>
                       </div>
-
-                      {/* EVENTUALE BOTTONE NOTIFICHE mobile */}
-
-                      {/* <button
-                        type="button"
-                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button> 
-                      */}
                     </div>
 
+                    {/* Profile Menu Buttons with Opacity Transition */}
                     <div
                       className={`mt-3 space-y-1 px-2 shadow-2xl w-2/5 rounded-lg transition-opacity ${
                         menuProfile ? "opacity-100" : "opacity-0"
