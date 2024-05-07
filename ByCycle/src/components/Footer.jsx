f1f1fce1995dcf8e60a938760f09a30de26e8734
Francesco <ballerifrancesco@gmail.com>
2024-05-07 17:49:59 +0200

    add modification to footer

--- ByCycle/src/components/Footer.jsx
+++ ByCycle/src/components/Footer.jsx
@@ -3,28 +3,30 @@ import { Button } from "./Button";
 import { Heading } from "./Heading";
 import { Img } from "./Img/Img";
 
+let year = new Date().getFullYear();
+
 export function Footer({ ...props }) {
   return (
     <footer
       {...props}
       className={`${props.className} flex flex-col items-center w-full pt-14 pb-[22px] gap-[55px] md:pt-5 sm:gap-[27px] sm:py-5 bg-white-A700`}
     >
-      <div className="container-xs flex flex-col gap-14 md:p-5 sm:gap-7 p-10">
-        <div className="flex items-start md:flex-col">
+      <div className="container-xs flex flex-col gap-14 md:p-5 sm:gap-7 p-10 ">
+        <div className="flex items-start md:flex-col ">
           <div className="flex flex-1 flex-col items-start gap-[11px] md:self-stretch">
             <Img
               src="images/img_footer_logo.png"
               alt="footer logo"
               className="h-[29px] w-[129px] object-contain"
             />
-            <p as="p" className="w-[49%] leading-[150%] md:w-full">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            <p as="p" className="w-[69%] leading-[150%] md:w-full">
+              "We value greatly that our community can have a place to share
+              their common passion and provide more information on cycling
+              routes that bring more excitement and safety during the journey.
+              Our team regularly commits to sharing updates regarding the world
+              of bike lovers. Our common goal is to ensure that our community
+              finds a place to share passions, information, experiences, and,
+              why not, even enjoy a bike ride together."
             </p>
           </div>
           <div className="flex w-[49%] items-start justify-between gap-5 md:w-full sm:flex-col p-5">
@@ -57,38 +59,6 @@ export function Footer({ ...props }) {
             </div>
             <div className="flex flex-col gap-2">
               <Heading as="h6" className="!font-bold">
-                Services
-              </Heading>
-              <ul className="flex flex-col gap-[9px]">
-                <li className="hover:text-emerald-500">
-                  <a href="Bussines" target="_blank" rel="noreferrer">
-                    <p as="p">Bussines</p>
-                  </a>
-                </li>
-                <li className="hover:text-emerald-500">
-                  <a href="Modifications" target="_blank" rel="noreferrer">
-                    <p as="p">Modifications</p>
-                  </a>
-                </li>
-                <li className="hover:text-emerald-500">
-                  <a href="#">
-                    <p as="p">ByCycle Care</p>
-                  </a>
-                </li>
-                <li className="hover:text-emerald-500">
-                  <a href="Insurance" target="_blank" rel="noreferrer">
-                    <p as="p">Insurance</p>
-                  </a>
-                </li>
-                <li className="hover:text-emerald-500">
-                  <a href="Warranty" target="_blank" rel="noreferrer">
-                    <p as="p">Warranty</p>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-2">
-              <Heading as="h6" className="!font-bold">
                 Resource
               </Heading>
               <ul className="flex flex-col gap-[9px]">
@@ -110,11 +80,6 @@ export function Footer({ ...props }) {
                 <li className="hover:text-emerald-500">
                   <a href="FAQS" target="_blank" rel="noreferrer">
                     <p as="p">FAQS</p>
-                  </a>
-                </li>
-                <li className="hover:text-emerald-500">
-                  <a href="Review" target="_blank" rel="noreferrer">
-                    <p as="p">Review</p>
                   </a>
                 </li>
               </ul>
@@ -151,20 +116,14 @@ export function Footer({ ...props }) {
                 Contact Us
               </Heading>
               <ul className="flex flex-col items-start gap-[9px]">
-                <li className="hover:text-emerald-500">
-                  <a href="#">
-                    <p as="p">Via Thailandia 6, Roma 00100 RM </p>
-                  </a>
+                <li>
+                  <p as="p">Via Thailandia 6, Roma 00100 RM </p>
                 </li>
-                <li className="hover:text-emerald-500">
-                  <a href="#">
-                    <p as="p">bycyclewebapp@email.it</p>
-                  </a>
+                <li>
+                  <p as="p">bycyclewebapp@email.it</p>
                 </li>
-                <li className="hover:text-emerald-500">
-                  <a href="Cookies" target="_blank" rel="noreferrer">
-                    <p as="p">+39 333 4445555</p>
-                  </a>
+                <li>
+                  <p as="p">+39 333 4445555</p>
                 </li>
               </ul>
             </div>
@@ -210,20 +169,21 @@ export function Footer({ ...props }) {
           </Button>
         </div>
       </div>
-      <div className="container-xs flex flex-col gap-[55px] md:p-5 sm:gap-[27px]">
-        <div className="h-px bg-gray-200" />
-        <div className="flex items-start md:flex-col">
+
+      <div className="container-xs flex flex-col w-full gap-[55px] md:p-5 sm:gap-[27px]">
+        <div className="h-px bg-gray-200 " />
+        <div className="flex items-start md:flex-col pl-5">
           <p size="xs" as="p">
-            ©2024 ByCycle All right reserved.
+            {`© ${year} ByCycle All right reserved.`}
           </p>
-          <Img
+          {/*  <Img
             src="images/img_television.svg"
             alt="television image"
             className="ml-[784px] h-[16px] w-[16%] md:ml-0 md:w-full"
           />
           <p size="xs" as="p" className="ml-2 !text-gray-500_02 md:ml-0">
             Base on 10.348 review
-          </p>
+          </p> */}
         </div>
       </div>
     </footer>
