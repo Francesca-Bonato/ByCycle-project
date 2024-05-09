778daa8fcc595b8dd966d679a6abfe34f4189fb6
Luigi Balestrucci <card3001@outlook.it>
2024-05-09 12:18:48 +0200

    Add hover effects on button

--- ByCycle/src/components/Button.jsx
+++ ByCycle/src/components/Button.jsx
@@ -1,6 +1,8 @@
 const Button = ({ innerText, className = "" }) => {
   return (
-    <button className={`${className} bg-black text-white min-w-[119px] h-[48px] px-8 font-semibold rounded-[24px]`}>
+    <button
+      className={`${className} bg-black text-white min-w-[119px] h-[48px] px-8 font-semibold rounded-[24px] hover:text-black hover:bg-white hover:border-black border border-solid transition-all`}
+    >
       {`${innerText}`}
     </button>
   );
--- ByCycle/src/main.jsx
+++ ByCycle/src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
 import Community from "./pages/Community";
 import BikeTrails from "./pages/BikeTrails";
-import NewsPage from "./pages/NewsPage";
+import Newspage from "./pages/Newspage";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import ContactUs from "./pages/ContactUs";
@@ -15,11 +15,11 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { path: "/", element: <Homepage /> },
-      { path: "/community", element: <Community/> },
-      { path: "/trails", element: <BikeTrails/> },
-      { path: "/news", element: <NewsPage /> },
+      { path: "/community", element: <Community /> },
+      { path: "/trails", element: <BikeTrails /> },
+      { path: "/news", element: <Newspage /> },
       { path: "/login", element: <Login /> },
-      { path: "/contacts", element: <ContactUs/> },
+      { path: "/contacts", element: <ContactUs /> },
     ],
   },
 ]);
