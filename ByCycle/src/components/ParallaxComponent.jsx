cc725bff043c9399fb7929bbfaf76ede078c2fc1
Sergio Gagliano <sergiogagliano8@gmail.com>
2024-05-10 16:11:22 +0200

    homepage update parallax

--- ByCycle/src/components/ParallaxComponent.jsx
+++ ByCycle/src/components/ParallaxComponent.jsx
@@ -17,19 +17,23 @@ export default function ParallaxComponent() {
         style={{
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
-          background: "#1f1f1f1f",
+          // "background-image": "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)",
+          "height": "800px",
+          "width" : "100vw",
         }}
-        speed={600}
+        speed={1200}
         parallax={true}
+        grabCursor={true}
+        effect={"cube"}
         pagination={{
-          clickable: true,
+        clickable: true,
         }}
         navigation={true}
         modules={[Parallax, Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="text-left bg-[url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)] bg-center bg-cover  text-white p-5">
+          <div className="text-left h-full bg-[url(https://fiabitalia.it/wp-content/uploads/2021/06/FIAB_Cicloturismo_2-copia-scaled.jpg)] bg-center bg-cover text-white p-5">
             <div className="title" data-swiper-parallax="-300">
               Slide 1
             </div>
@@ -50,7 +54,7 @@ export default function ParallaxComponent() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="text-left bg-[url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)] bg-center bg-cover  text-white p-5">
+          <div className="text-left  h-full bg-[url(https://www.comune.desenzano.brescia.it/var/desenzano/storage/images/media/images/itinerari-ciclabili/693094-2-ita-IT/Itinerari-ciclabili_reference.jpg)] bg-center bg-contein bg-no-repeat  text-white p-5">
             <div className="title" data-swiper-parallax="-300">
               Slide 1
             </div>
@@ -71,7 +75,7 @@ export default function ParallaxComponent() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="text-left bg-[url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)] bg-center bg-cover  text-white p-5">
+          <div className="text-left h-full  bg-[url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)] bg-center bg-cover  text-white p-5">
             <div className="title" data-swiper-parallax="-300">
               Slide 1
             </div>
