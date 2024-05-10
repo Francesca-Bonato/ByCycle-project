c513546671775322981c33cf2abd7d85a6a43d75
Sergio Gagliano <sergiogagliano8@gmail.com>
2024-05-10 18:32:35 +0200

    hompage completa

--- ByCycle/src/components/ParallaxComponent.jsx
+++ ByCycle/src/components/ParallaxComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -17,31 +18,39 @@ export default function ParallaxComponent() {
         style={{
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
-          // "background-image": "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)",
-          "height": "800px",
-          "width" : "100vw",
+          height: "800px",
         }}
         speed={1200}
         parallax={true}
         grabCursor={true}
         effect={"cube"}
         pagination={{
-        clickable: true,
+          clickable: true,
         }}
         navigation={true}
         modules={[Parallax, Pagination, Navigation]}
-        className="mySwiper"
       >
+        
+
         <SwiperSlide>
-          <div className="text-left h-full bg-[url(https://fiabitalia.it/wp-content/uploads/2021/06/FIAB_Cicloturismo_2-copia-scaled.jpg)] bg-center bg-cover text-white p-5">
-            <div className="title" data-swiper-parallax="-300">
-              Slide 1
+          <div className="text-left flex flex-col gap-5 h-full bg-[url(https://fiabitalia.it/wp-content/uploads/2021/06/FIAB_Cicloturismo_2-copia-scaled.jpg)] bg-center bg-contein bg-no-repeat  text-white p-5">
+            <div
+              className="title text-[40px] text-center text-gray-900"
+              data-swiper-parallax="-300"
+            >
+              Questionnaire 2
             </div>
-            <div className="subtitle" data-swiper-parallax="-200">
+            <div
+              className="subtitle bubble left w-auto text-center text-gray-900"
+              data-swiper-parallax="-200"
+            >
               Subtitle
             </div>
-            <div className="text" data-swiper-parallax="-100">
-              <p>
+            <div
+              className="text w-auto lg:w-[800px] text-center text-white"
+              data-swiper-parallax="-100"
+            >
+              <p className="bubble left break-words">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
                 laoreet justo vitae porttitor porttitor. Suspendisse in sem
@@ -50,19 +59,69 @@ export default function ParallaxComponent() {
                 ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
                 tincidunt ut libero. Aenean feugiat non eros quis feugiat.
               </p>
+              <Link to="/quiz_two">
+                <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
+                  Go to the questionnaire 💬🚴
+                </button>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="text-left flex flex-col gap-5 h-full bg-[url(https://www.comune.desenzano.brescia.it/var/desenzano/storage/images/media/images/itinerari-ciclabili/693094-2-ita-IT/Itinerari-ciclabili_reference.jpg)] bg-center bg-contein bg-no-repeat  text-white p-5">
+            <div
+              className="title text-[40px] text-center text-gray-900"
+              data-swiper-parallax="-300"
+            >
+              Questionnaire 2
+            </div>
+            <div
+              className="subtitle bubble left w-auto text-center text-gray-900"
+              data-swiper-parallax="-200"
+            >
+              Subtitle
+            </div>
+            <div
+              className="text w-auto lg:w-[800px] text-center text-white"
+              data-swiper-parallax="-100"
+            >
+              <p className="bubble left break-words">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                laoreet justo vitae porttitor porttitor. Suspendisse in sem
+                justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
+                euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+                ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+                tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              </p>
+              <Link to="/quiz_two">
+                <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
+                  Go to the questionnaire 💬🚴
+                </button>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="text-left  h-full bg-[url(https://www.comune.desenzano.brescia.it/var/desenzano/storage/images/media/images/itinerari-ciclabili/693094-2-ita-IT/Itinerari-ciclabili_reference.jpg)] bg-center bg-contein bg-no-repeat  text-white p-5">
-            <div className="title" data-swiper-parallax="-300">
-              Slide 1
+          <div className="text-left flex flex-col gap-5 h-full  bg-[url(https://www.fiabverona.it/wp-content/uploads/2021/12/RL172-Tour-dei-cento-laghi-viaggio-in-bici-tra-cultura-e-natura-Lago-Maggiore.jpg)] bg-center bg-cover  text-white p-5">
+            <div
+              className="title text-[40px] text-center text-white"
+              data-swiper-parallax="-300"
+            >
+              Questionnaire 3
             </div>
-            <div className="subtitle" data-swiper-parallax="-200">
+            <div
+              className="subtitle bubble left w-auto text-center text-white"
+              data-swiper-parallax="-200"
+            >
               Subtitle
             </div>
-            <div className="text" data-swiper-parallax="-100">
-              <p>
+            <div
+              className="text w-auto lg:w-[800px] text-center text-white"
+              data-swiper-parallax="-100"
+            >
+              <p className="bubble left break-words">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
                 laoreet justo vitae porttitor porttitor. Suspendisse in sem
@@ -71,27 +130,11 @@ export default function ParallaxComponent() {
                 ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
                 tincidunt ut libero. Aenean feugiat non eros quis feugiat.
               </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="text-left h-full  bg-[url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D)] bg-center bg-cover  text-white p-5">
-            <div className="title" data-swiper-parallax="-300">
-              Slide 1
-            </div>
-            <div className="subtitle" data-swiper-parallax="-200">
-              Subtitle
-            </div>
-            <div className="text" data-swiper-parallax="-100">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                laoreet justo vitae porttitor porttitor. Suspendisse in sem
-                justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
-                euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-              </p>
+              <Link to="/quiz_three">
+                <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
+                  Go to the questionnaire 💬🚴
+                </button>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
