9a78916e4b0ccc130c39d8f735707dcf9bde8146
Francesco <ballerifrancesco@gmail.com>
2024-05-21 18:31:21 +0200

    fixed logic on reg and login

--- ByCycle/src/components/NavBar.jsx
+++ ByCycle/src/components/NavBar.jsx
@@ -133,7 +133,7 @@ export function NavBar() {
                           leaveTo="opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {localStorage.getItem("Utente")
+                            {sessionStorage.getItem(user.email)
                               ? logged.map((item) => (
                                   <Menu.Item key={item.name}>
                                     {({ active }) => (
@@ -243,7 +243,7 @@ export function NavBar() {
                           menuProfile ? "opacity-100" : "opacity-0"
                         }`}
                       >
-                        {localStorage.getItem("Utente")
+                        {sessionStorage.getItem(user.email)
                           ? logged.map((item) => (
                               <Disclosure.Button
                                 key={item.name}
