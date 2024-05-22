f0d385a335c315ebe7211048875960a320744c3f
Francesca Bonato <francesca.bonato@hotmail.com>
2024-05-22 18:09:03 +0200

    improvements design parallax

--- ByCycle/src/components/ParallaxComponent.jsx
+++ ByCycle/src/components/ParallaxComponent.jsx
@@ -34,14 +34,14 @@ export default function ParallaxComponent() {
         {/* These SwiperSlides contain the content for each slide */}
         {/* First SwiperSlide for quiz 1 */}
         <SwiperSlide>
-          <div className="w-full flex flex-col items-start justify-center gap-6 h-full bg-gradient-quiz-one bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
-            <h3 className="text-[40px] ml-[50px] text-left leading-[150%] max-w-[500px]">
+          <div className="w-full flex flex-col sm:items-start justify-center gap-6 h-full bg-gradient-quiz-one bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
+            <h2 className="sm:ml-[50px] text-center sm:text-left leading-[150%] max-w-[500px]">
             Ready to Ride Through History?
-            </h3>
-            <p className="text-[20px] ml-[50px] text-left max-w-[500px]" data-swiper-parallax="-200">
+            </h2>
+            <p className="px-3 sm:px-0 sm:ml-[50px] text-center sm:text-left max-w-[500px]" data-swiper-parallax="-200">
             Brush up on your cycling knowledge! From inventors to maintenance, this quiz tests your expertise.
             </p>
-            <div className="ml-[50px] " data-swiper-parallax="-100">
+            <div className="sm:ml-[50px] " data-swiper-parallax="-100">
               <Link to="/quiz/1">
                 <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
                 Test your skills 🚴💬
@@ -52,14 +52,14 @@ export default function ParallaxComponent() {
         </SwiperSlide>
         {/* Second SwiperSlide for quiz 2 */}
         <SwiperSlide>
-          <div className="w-full flex flex-col items-start justify-center gap-6 h-full bg-gradient-quiz-two bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
-            <h3 className="text-[40px] ml-[50px] text-left leading-[150%] max-w-[500px]">
+          <div className="w-full flex flex-col sm:items-start justify-center gap-6 h-full bg-gradient-quiz-two bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
+            <h2 className="sm:ml-[50px] text-center sm:text-left  leading-[150%] max-w-[500px]">
             Would you call yourself a master mechanic?
-            </h3>
-            <p className="text-[20px] ml-[50px] text-left max-w-[500px]" data-swiper-parallax="-200">
+            </h2>
+            <p className="px-3 sm:px-0 sm:ml-[50px] text-center sm:text-left max-w-[500px]" data-swiper-parallax="-200">
             Gear up for a challenge! This quiz puts your knowledge of bicycle history, maintenance, and riding techniques to the test.
             </p>
-            <div className="ml-[50px] " data-swiper-parallax="-100">
+            <div className="sm:ml-[50px] " data-swiper-parallax="-100">
               <Link to="/quiz/2">
                 <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
                 Test your skills 🚴💬
@@ -70,14 +70,14 @@ export default function ParallaxComponent() {
         </SwiperSlide>
         {/* Third SwiperSlide for quiz 3 */}
         <SwiperSlide>
-          <div className="w-full flex flex-col items-start justify-center gap-6 h-full bg-gradient-quiz-three bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
-            <h3 className="text-[40px] ml-[50px] text-left leading-[150%] max-w-[500px]">
+          <div className="w-full flex flex-col sm:items-start justify-center gap-6 h-full bg-gradient-quiz-three bg-center bg-contein bg-no-repeat  text-white p-10 2xl:text-lg">
+            <h2 className="sm:ml-[50px] text-center sm:text-left leading-[150%] max-w-[500px]">
            Do you REALLY know how to ride?
-            </h3>
-            <p className="text-[20px] ml-[50px] text-left max-w-[500px]" data-swiper-parallax="-200">
+            </h2>
+            <p className="px-3 sm:px-0 sm:ml-[50px] text-center sm:text-left max-w-[500px]" data-swiper-parallax="-200">
             No matter your experience level, this quiz has something for you! Test your knowledge and become a more confident rider.
             </p>
-            <div className="ml-[50px] " data-swiper-parallax="-100">
+            <div className="sm:ml-[50px] " data-swiper-parallax="-100">
               <Link to="/quiz/3">
                 <button className="bg-[#232323] text-white pt-3 pb-3 pl-8 pr-8 rounded-[40px]  mt-10 border transition hover:border-solid hover:border-[#232323] hover:bg-white hover:text-black">
                   Test your skills 🚴💬
@@ -87,6 +87,7 @@ export default function ParallaxComponent() {
           </div>
         </SwiperSlide>
       </Swiper>
+      <div className="h-[50px]"></div>
     </>
   );
 }
