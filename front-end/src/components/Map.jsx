b007bf0a6abf14a5b3028f361782b32f476d1926
Luigi Balestrucci <card3001@outlook.it>
2024-06-09 17:32:58 +0200

    Fix to 80% Trails Map

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -1,57 +1,54 @@
-// src/components/Map.js
-
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 const Map = () => {
-  const mapRef = useRef(null);
+  const [location, setLocation] = useState({ lat: null, lng: null });
+  const [error, setError] = useState("");
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        initMap(latitude, longitude);
-      }, showError);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (err) => {
+          setError("Geolocalizzazione non è supportata o permesso negato");
+          console.error(err);
+        }
+      );
     } else {
-      alert("Geolocalizzazione non supportata dal tuo browser.");
+      setError("Geolocalizzazione non è supportata dal tuo browser.");
     }
   }, []);
 
-  const showError = (error) => {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        alert("L'utente ha negato la richiesta di geolocalizzazione.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Le informazioni sulla posizione non sono disponibili.");
-        break;
-      case error.TIMEOUT:
-        alert("La richiesta per ottenere la posizione è scaduta.");
-        break;
-      default:
-        alert("Si è verificato un errore sconosciuto.");
-        break;
-    }
-  };
-
-  const initMap = (latitude, longitude) => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: { lat: latitude, lng: longitude },
-      zoom: 14,
-    });
-
-    // Aggiungi il livello di percorsi ciclabili
-    const bikeLayer = new window.google.maps.BicyclingLayer();
-    bikeLayer.setMap(map);
-  };
-
   return (
     <div>
-      <h1>Mappa Dinamica con Percorsi Ciclabili</h1>
-      <div
-        ref={mapRef}
-        id="map"
-        style={{ width: "100%", height: "500px" }}
-      ></div>
+      {location.lat && location.lng ? (
+        <iframe
+          title="Google Maps"
+          width="100%"
+          height="700px"
+          frameBorder="0"
+          style={{ border: 0 }}
+          //DEBUG CODE MAP      
+          //       <iframe
+          //   width="100%"
+          //   height="100%"
+          //   frameBorder="0"
+          //   marginHeight="0"
+          //   marginWidth="0"
+          //   title="map"
+          //   scrolling="no"
+          //   src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6429019.961973948!2d7.877651878918112!3d38.1129815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spercorsi%20ciclabili!5e0!3m2!1sit!2sit!4v1716817299018!5m2!1sit!2sit"
+          // ></iframe>
+          src={`https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d68019.961973948!2d${location.lng}!3d${location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spercorsi%20ciclabili!5e0!3m2!1sit!2sit!4v1716817299018!5m2!1sit!2sit`}
+          allowFullScreen=""
+        ></iframe>
+      ) : (
+        <p>{error || "Caricamento della tua posizione..."}</p>
+      )}
     </div>
   );
 };
--- front-end/src/components/TrailsMap.jsx
+++ front-end/src/components/TrailsMap.jsx
@@ -4,19 +4,6 @@ function TrailsMap() {
   return (
     <section className="text-gray-600 body-font relative mb-5">
       <div className="absolute inset-0 bg-gray-300">
-        {/*  -----------------------------------------------------------------------------------------------
-         //PREVIOUS MAPS GENERIC
-          <iframe
-            width="100%"
-            height="100%"
-            frameBorder="0"
-            marginHeight="0"
-            marginWidth="0"
-            title="map"
-            scrolling="no"
-            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6429019.961973948!2d7.877651878918112!3d38.1129815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spercorsi%20ciclabili!5e0!3m2!1sit!2sit!4v1716817299018!5m2!1sit!2sit"
-          ></iframe>
-        --------------------------------------------------------------------------------------------------- */}
         <Map />
       </div>
       <div className="container px-5 py-24 mx-auto flex">
