71f875b249ff0920db3c1c13a9501d038bba6eb6
Francesco <ballerifrancesco@gmail.com>
2024-06-10 12:20:32 +0200

    fixed img insert into db

--- front-end/src/pages/EventDetails.jsx
+++ front-end/src/pages/EventDetails.jsx
@@ -32,17 +32,20 @@ function EventDetails() {
     }
   };
 
-const formattedStartDate = new Date(event.startDate).toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-});
+  const formattedStartDate = new Date(event.startDate).toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
 
-const formattedEndDate = new Date(event.endDate).toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-});
+  const formattedEndDate = new Date(event.endDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   // Render event details if the event is found
   return (
@@ -51,40 +54,46 @@ const formattedEndDate = new Date(event.endDate).toLocaleDateString('en-US', {
         <p>Loading event details...</p>
       ) : error ? (
         <p>Error fetching event details: {error.message}</p>
-      ) : event && (
-        <>
-          {/* Main container with responsive padding */}
-          <section className="container-xs flex items-center justify-center flex-col max-w-[1260px] mx-auto py-16 px-5 md:px-10 lg:px-16 2xl:text-lg">
-            {/* Event title */}
-            <h3 className="text-center text-neutral-800 font-medium leading-[55px] mb-3">
-              {event.title}
-            </h3>
-            {/* Flexible container for image and text*/}
-            <div className="flex flex-col-reverse items-center justify-center gap-10 flex-wrap lg:flex-row lg:flex-nowrap lg:justify-between">
-              {/* Event image */}
-              <div className="w-full lg:w-auto">
-                <img
-                  src={event.img}
-                  alt={event.title}
-                  className="w-full h-[300px] object-top rounded-[16px] object-cover sm:object-center lg:h-100 lg:h-[560px]"
-                />
+      ) : (
+        event && (
+          <>
+            {/* Main container with responsive padding */}
+            <section className="container-xs flex items-center justify-center flex-col max-w-[1260px] mx-auto py-16 px-5 md:px-10 lg:px-16 2xl:text-lg">
+              {/* Event title */}
+              <h3 className="text-center text-neutral-800 font-medium leading-[55px] mb-3">
+                {event.title}
+              </h3>
+              {/* Flexible container for image and text*/}
+              <div className="flex flex-col-reverse items-center justify-center gap-10 flex-wrap lg:flex-row lg:flex-nowrap lg:justify-between">
+                {/* Event image */}
+                <div className="w-full h-full lg:w-auto">
+                  <img
+                    src={event.img}
+                    alt={event.title}
+                    className="w-full h-[300px] object-top rounded-[16px] object-cover sm:object-center lg:h-100 lg:h-[560px]"
+                  />
+                </div>
+                <div className="flex flex-col items-center my-[10px] justify-center gap-5 break-words w-full lg:w-[42%]">
+                  {/* Event description and other information */}
+                  <ul className="text-zinc-500 text-justify md:font-semibold py-8">
+                    <li className="pb-8">{event.description}</li>
+                    <li className="font-bold">
+                      Location: {event.eventLocation}
+                    </li>
+                    <li className="font-bold">
+                      Start Date: {formattedStartDate}
+                    </li>
+                    <li className="font-bold">End Date: {formattedEndDate}</li>
+                    <li className="font-bold">Level: {event.level}</li>
+                  </ul>
+                </div>
               </div>
-              <div className="flex flex-col items-center my-[10px] justify-center gap-5 break-words w-full lg:w-[42%]">
-                {/* Event description and other information */}
-                <ul className="text-zinc-500 text-justify md:font-semibold py-8">
-                  <li className="pb-8">{event.description}</li>
-                  <li className="font-bold">Location: {event.eventLocation}</li>
-                  <li className="font-bold">Start Date: {formattedStartDate}</li>
-                  <li className="font-bold">End Date: {formattedEndDate}</li>
-                  <li className="font-bold">Level: {event.level}</li>
-                </ul>
-              </div>
-            </div>
-            <Link to="/events">
-              <Button innerText="←  Back to all events " className="mt-6"/>
-            </Link>
-          </section>
-        </>
+              <Link to="/events">
+                <Button innerText="←  Back to all events " className="mt-6" />
+              </Link>
+            </section>
+          </>
+        )
       )}
     </>
   );
